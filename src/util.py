84de1a618e19376c2c1f42a48ac4975228b4b927
Jonda <jon.tich@volny.cz>
2021-10-19 23:00:14 +0200

    patch np done

--- src/util.py
+++ src/util.py
@@ -1,6 +1,6 @@
 import requests
 from archive import extract
-import tarfile
+import numpy as np
 
 def fetch_dataset():
     url = 'https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/segbench/BSDS300-images.tgz'
@@ -12,3 +12,21 @@ def fetch_dataset():
             f.write(response.raw.read())
     # archive = tarfile.open('../data/BSDS300-images.tgz' , "r:*")
     # archive.extract('images/train', '../data/train')
+
+def Im2PatchNP(img, win, stride=1):
+    '''
+    Patch creating function, input dataset must be of numpy array format
+    '''
+    k = 0
+    endc = img.shape[2]
+    endw = img.shape[1]
+    endh = img.shape[0]
+    patch = img[0:endh-win+0+1:stride, 0:endw-win+0+1:stride, :]
+    TotalPatNum = patch.shape[0] * patch.shape[1]
+    Y = np.zeros([win*win,TotalPatNum, endc], np.float32)
+    for i in range(win):
+        for j in range(win):
+            patch = img[i:endh-win+i+1:stride, j:endw-win+j+1:stride, :]
+            Y[k,:,:] = np.array(patch[:]).reshape(TotalPatNum, endc)
+            k = k + 1
+    return Y.reshape([win, win, TotalPatNum, endc])
