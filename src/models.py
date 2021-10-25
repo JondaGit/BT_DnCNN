d7cdc61084a770b3a6ba548e7467877bedd882af
Jonda <jon.tich@volny.cz>
2021-10-25 22:51:47 +0200

    new patching

--- src/models.py
+++ src/models.py
@@ -3,6 +3,9 @@ import os
 from torch.utils.data import Dataset
 from PIL import Image
 from torchvision import transforms
+from util import Im2PatchNP
+import random
+import numpy as np
 
 class DnCNN(nn.Module):
     def __init__(self):
@@ -27,19 +30,24 @@ class DnCNN(nn.Module):
         return out
 
 class ImageDataset(Dataset):
-  def __init__(self, image_dir, input_transforms=None):
+  def __init__(self, image_dir, patch_size=40):
       super(ImageDataset, self).__init__()
       self.image_filenames = [os.path.join(image_dir, x) for x in os.listdir(image_dir) if is_image_file(x)]
-      self.input_transforms = input_transforms
+      self.patch_size = patch_size
 
   def __getitem__(self, index):
-      input = Image.open(self.image_filenames[index])
+      input = np.asarray(Image.open(self.image_filenames[index]))
+      H, W = input.shape[:2]
+      rnd_h = random.randint(0, max(0, H - self.patch_size))
+      rnd_w = random.randint(0, max(0, W - self.patch_size))
 
-      transform = transforms.Compose([
-            transforms.Resize((256,256)),
-            transforms.PILToTensor()
-        ])
-      return transform(input)
+      patch = input[rnd_h:rnd_h + self.patch_size, rnd_w:rnd_w + self.patch_size, :]
+    #   transform = transforms.Compose([
+    #         transforms.Resize((256,256)),
+    #         transforms.PILToTensor()
+    #     ])
+    
+      return transforms.PILToTensor()(Image.fromarray(patch))
   
   def __len__(self):
       return len(self.image_filenames)
